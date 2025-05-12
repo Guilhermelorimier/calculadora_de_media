d00a26c78205a05d279dcd7940270bf15a7dad80
Guilhermelorimier <guilhermeaugustolorimier@gmail.com>
2025-05-12 16:47:00 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,8 @@
+nome = input("digite o nome do aluno")
+
+nota1 = float(input("digite a primeira nota: "))
+nota2 = float(input("digite a segunda nota:"))          
+nota3 = float(input("digite a terceira nota"))
+
+media = (nota1 + nota2 + nota3 ) / 3
+print(f"media  do aluno é: {media:.1f}")
